c0061c3f78c2b602bcf9d8f7f84814cf8ad111c3
Krinza Momin <44579545+kaymomin@users.noreply.github.com>
2022-06-15 01:12:59 +0500

    Update KittyMonster.sol

--- contracts/KittyMonster.sol
+++ contracts/KittyMonster.sol
@@ -6,7 +6,6 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-
 contract KittyMonster is ERC721URIStorage, Ownable {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
